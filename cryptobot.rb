30438f4b4a8f7888d117de2e7354e81ba036c5e5
Dimitrijus <dimitrijusstepanov@gmail.com>
2021-01-12 21:55:11 +0200

    Add command to get current BTC price

--- cryptobot.rb
+++ cryptobot.rb
@@ -1,10 +1,26 @@
 require 'discordrb'
 require 'dotenv/load'
+require 'faraday'
+
+BASE_URL = 'https://pro-api.coinmarketcap.com/v1/tools/price-conversion'.freeze
 
 bot = Discordrb::Commands::CommandBot.new token: ENV['BOT_TOKEN'], prefix: '!'
 
-bot.command :me do |event|
-    event.user.name
-  end
+bot.command :btc do |event|
+  params = { symbol: 'BTC', amount: 1 }
+
+  response = Faraday.get(BASE_URL, params, { 'X-CMC_PRO_API_KEY' => ENV['COINMARKET_API_KEY']})
+
+  return unless response.success?
+
+  response_body = JSON.parse(response.body, symbolize_names: true)
+
+  data = response_body[:data][:quote][:USD]
+  price = data[:price]
+  last_updated = data[:last_updated]
+
+  event << "Current BTC price is: #{price} USD"
+  event << "Last updated at: #{last_updated}"
+end
 
 bot.run
