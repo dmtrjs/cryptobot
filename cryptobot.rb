6918f2be39a139a75f3b37fe168b422f3bd17138
Dimitrijus <dimitrijusstepanov@gmail.com>
2021-01-13 00:56:19 +0200

    Send scheduled price notificaitons to hardcoded channel

--- cryptobot.rb
+++ cryptobot.rb
@@ -1,14 +1,13 @@
 require 'discordrb'
 require 'dotenv/load'
 require 'faraday'
+require 'rufus-scheduler'
+
+scheduler = Rufus::Scheduler.new
 
 BASE_URL = 'https://pro-api.coinmarketcap.com/v1/tools/price-conversion'.freeze
 
-bot = Discordrb::Commands::CommandBot.new token: ENV['BOT_TOKEN'], prefix: '!'
-
-puts "Invite url: #{bot.invite_url}"
-
-bot.command :btc do |event|
+def send_embed_to_channel(channel)
   params = { symbol: 'BTC', amount: 1 }
 
   response = Faraday.get(BASE_URL, params, { 'X-CMC_PRO_API_KEY' => ENV['COINMARKET_API_KEY']})
@@ -17,11 +16,11 @@ bot.command :btc do |event|
 
   response_body = JSON.parse(response.body, symbolize_names: true)
 
-  data = response_body[:data][:quote][:USD]
-  price = data[:price]
-  last_updated = data[:last_updated]
+  channel.send_embed do |embed|
+    data = response_body[:data][:quote][:USD]
+    price = data[:price]
+    last_updated = data[:last_updated]
 
-  event.channel.send_embed do |embed|
     embed.colour = '#0099ff'
     embed.title = 'BTC Price'
     embed.author = Discordrb::Webhooks::EmbedAuthor.new(name: 'Crypto Bot',
@@ -31,4 +30,24 @@ bot.command :btc do |event|
   end
 end
 
-bot.run
+bot = Discordrb::Commands::CommandBot.new token: ENV['BOT_TOKEN'], prefix: '!'
+
+puts "Invite url: #{bot.invite_url}"
+
+bot.command :btc do |event|
+  send_embed_to_channel(event.channel)
+end
+
+bot.run :async
+
+channels = bot.find_channel('crypto-prices')
+
+scheduler.every '10h' do
+  if channels.any?
+    channels.each do |channel|
+      send_embed_to_channel(channel)
+    end
+  end
+end
+
+bot.join
